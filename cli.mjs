ea49203d469b1b1d0bcb57be5b5039bb14c5bd05
Junxiao Shi <git@mail1.yoursunny.com>
2022-05-02 13:26:13 +0000

    v0.48.4

---
+++ cli.mjs
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import { execaNode } from "execa";
+import { createRequire } from "node:module";
+import { dirname, resolve } from "node:path";
+
+const require = createRequire(import.meta.url);
+const selfPath = dirname(require.resolve("."));
+const xoPath = dirname(require.resolve("xo"));
+
+process.exit((await execaNode(resolve(xoPath, "cli.js"), process.argv.slice(2), {
+  env: {
+    NODE_PATH: [
+      resolve(selfPath, "node_modules"),
+      resolve(selfPath, "../.."),
+      resolve(xoPath, "node_modules"),
+      resolve(xoPath, ".."),
+    ].join(":"),
+  },
+  reject: false,
+  stdin: "inherit",
+  stdout: "inherit",
+  stderr: "inherit",
+})).exitCode);
